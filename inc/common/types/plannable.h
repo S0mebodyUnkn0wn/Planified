d24ca46912c02a658085929f23366e8eb97eb16a
Daniil Guryev <Daniel.Guryev@gmail.com>
2025-01-05 00:14:26 +0200

    Define the `Plannable` interface

--- inc/common/types/plannable.h
+++ inc/common/types/plannable.h
@@ -1,8 +1,24 @@
-//
-// Created by somebody on 09/12/24.
-//
-
 #ifndef PLANNABLE_H
 #define PLANNABLE_H
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define PLANIFIED_TYPE_PLANNABLE planified_plannable_get_type()
+G_DECLARE_INTERFACE(PlanifiedPlannable, planified_plannable, PLANIFIED, PLANNABLE, GObject)
+
+
+struct _PlanifiedPlannableInterface
+{
+	GTypeInterface parent_iface;
+	bool (*plan)(PlanifiedPlannable* self,
+	             GDateTime* date_time);
+	bool (*unplan)(PlanifiedPlannable* self);
+};
+
+bool planified_plannable_plan(PlanifiedPlannable* self,GDateTime *date_time);
+bool planified_plannable_unplan(PlanifiedPlannable* self);
+
+G_END_DECLS
 
 #endif //PLANNABLE_H
--- src/common/types/plannable.c
+++ src/common/types/plannable.c
@@ -1,1 +1,43 @@
 #include <common/types/plannable.h>
+
+G_DEFINE_INTERFACE(PlanifiedPlannable, planified_plannable, G_TYPE_OBJECT)
+
+
+static void
+planified_plannable_default_init(PlanifiedPlannableInterface* iface)
+{
+	g_object_interface_install_property(
+		iface,
+		g_param_spec_boxed(
+			"start-time",
+			"Start time",
+			"The start time of the planned item",
+			G_TYPE_DATE_TIME,
+			G_PARAM_READWRITE
+		)
+	);
+	g_object_interface_install_property(
+		iface,
+		g_param_spec_uint64(
+			"duration",
+			"Duration",
+			"The duration of the planned item in seconds",
+			0,
+			INT64_MAX,
+			0,
+			G_PARAM_READWRITE
+		)
+	);
+}
+
+bool
+planified_plannable_unplan(PlanifiedPlannable* self)
+{
+	PlanifiedPlannableInterface *iface;
+	g_assert(PLANIFIED_IS_PLANNABLE(self));
+	g_return_val_if_fail (PLANIFIED_IS_PLANNABLE (self), false);
+
+	iface = PLANIFIED_PLANNABLE_GET_IFACE (self);
+	g_return_val_if_fail (iface->unplan != NULL, false);
+	return iface->unplan(self);
+}
