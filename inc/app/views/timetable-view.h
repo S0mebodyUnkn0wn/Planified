1fca640940df48bb4881649d8970f6ed791cd194
Daniil Guryev <Daniel.Guryev@gmail.com>
2025-01-26 15:42:52 +0000

    Create a placeholder for timetable view

--- inc/app/views/timetable-view.h
+++ inc/app/views/timetable-view.h
@@ -1,8 +1,16 @@
-//
-// Created by somebody on 09/12/24.
-//
-
 #ifndef TIMETABLE_VIEW_H
 #define TIMETABLE_VIEW_H
 #include <gtk/gtk.h>
+#include "common/types/types.h"
+
+G_BEGIN_DECLS
+
+#define PLANIFIED_TYPE_TIMETABLE_VIEW (planified_timetable_view_get_type())
+G_DECLARE_FINAL_TYPE(PlanifiedTimetableView, planified_timetable_view, PLANIFIED, TIMETABLE_VIEW, GtkBox)
+
+PlanifiedTimetableView*
+planified_timetable_view_new();
+
+G_END_DECLS
+
 #endif //TIMETABLE_VIEW_H
--- src/app/views/timetable-view.c
+++ src/app/views/timetable-view.c
@@ -1,1 +1,27 @@
 #include <app/views/timetable-view.h>
+
+struct _PlanifiedTimetableView
+{
+	GtkBox parent;
+
+};
+
+G_DEFINE_FINAL_TYPE(PlanifiedTimetableView, planified_timetable_view, GTK_TYPE_BOX)
+
+static void
+planified_timetable_view_init(PlanifiedTimetableView* self)
+{
+	gtk_widget_init_template(GTK_WIDGET(self));
+}
+
+static void
+planified_timetable_view_class_init(PlanifiedTimetableViewClass* klass)
+{
+	gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS(klass),"/planified/app/timetable-view.ui");
+}
+
+PlanifiedTimetableView*
+planified_timetable_view_new()
+{
+	return g_object_new(PLANIFIED_TYPE_TIMETABLE_VIEW,nullptr);
+}
