ab2405c88538a9529806c2269d46e34e65ab0956
Daniil Guryev <Daniel.Guryev@gmail.com>
2025-01-26 15:42:10 +0000

    Implement PlanifiedEntryContainer

--- inc/app/widgets/containers/entry-container.h
+++ inc/app/widgets/containers/entry-container.h
@@ -5,4 +5,24 @@
 #ifndef ENTRY_CONTAINER_H
 #define ENTRY_CONTAINER_H
 #include <gtk/gtk.h>
+#include "common/types/types.h"
+
+
+G_BEGIN_DECLS
+
+#define PLANIFIED_TYPE_ENTRY_CONTAINER planified_entry_container_get_type()
+G_DECLARE_DERIVABLE_TYPE(PlanifiedEntryContainer, planified_entry_container, PLANIFIED, ENTRY_CONTAINER, GtkBox)
+
+struct _PlanifiedEntryContainerClass
+{
+	GtkBoxClass parent_class;
+};
+
+PlanifiedEntryContainer*
+planified_entry_container_new(void);
+
+void
+planified_entry_container_set_entry(PlanifiedEntryContainer* self, PlanifiedEntry* entry);
+
+G_END_DECLS
 #endif //ENTRY_CONTAINER_H
--- src/app/widgets/containers/entry-container.c
+++ src/app/widgets/containers/entry-container.c
@@ -1,1 +1,54 @@
-#include <app/widgets/containers/entry-container.h>
+#include "app/widgets/containers/entry-container.h"
+
+typedef struct
+{
+	PlanifiedEntry* entry;
+	GtkLabel* title;
+	GtkLabel* description;
+	GtkCheckButton* complete_button;
+} PlanifiedEntryContainerPrivate;
+
+G_DEFINE_TYPE_WITH_PRIVATE(PlanifiedEntryContainer, planified_entry_container, GTK_TYPE_BOX)
+
+static void
+planified_entry_container_init(PlanifiedEntryContainer* self)
+{
+	gtk_widget_init_template(GTK_WIDGET(self));
+}
+
+void planified_entry_container_set_entry(PlanifiedEntryContainer* self, PlanifiedEntry* entry)
+{
+	PlanifiedEntryContainerPrivate* priv = planified_entry_container_get_instance_private(self);
+	if (priv->entry != nullptr)
+		g_object_unref(priv->entry);
+	priv->entry = entry;
+	if (priv->entry != nullptr)
+	{
+		g_object_ref(priv->entry);
+		g_object_bind_property(entry, "title", priv->title, "label", G_BINDING_SYNC_CREATE);
+		g_object_bind_property(entry, "description", priv->description, "label", G_BINDING_SYNC_CREATE);
+	}
+	if (PLANIFIED_IS_TASK(priv->entry))
+	{
+		gtk_widget_set_visible(GTK_WIDGET(priv->complete_button),true);
+	}
+	else
+	{
+		gtk_widget_set_visible(GTK_WIDGET(priv->complete_button),false);
+	}
+}
+
+static void
+planified_entry_container_class_init(PlanifiedEntryContainerClass* klass)
+{
+	gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS(klass), "/planified/app/entry-container.ui");
+
+	gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS(klass), PlanifiedEntryContainer, title);
+	gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS(klass), PlanifiedEntryContainer, description);
+	gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS(klass), PlanifiedEntryContainer, complete_button);
+}
+
+PlanifiedEntryContainer* planified_entry_container_new()
+{
+	return g_object_new(PLANIFIED_TYPE_ENTRY_CONTAINER, nullptr);
+}
