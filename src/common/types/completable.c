41358e82b2a4700bb8f50ac9de113691c0b3c355
Daniil Guryev <Daniel.Guryev@gmail.com>
2025-01-26 15:40:13 +0000

    Finish implementing completable props

--- src/common/types/completable.c
+++ src/common/types/completable.c
@@ -15,4 +15,42 @@ planified_completable_default_init(PlanifiedCompletableInterface* iface)
 			G_PARAM_READWRITE
 		)
 	);
+	g_object_interface_install_property(
+		iface,
+		g_param_spec_boxed(
+			"deadline",
+			"Deadline",
+			"A time by which the item should be completed, can be null",
+			G_TYPE_DATE_TIME,
+			G_PARAM_READWRITE
+		)
+	);
+	g_object_interface_install_property(
+		iface,
+		g_param_spec_enum(
+			"deadline-precision",
+			"Deadline precision",
+			"How precise is the deadline property",
+			PLANIFIED_TYPE_TIME_PRECISION,
+			PLANIFIED_TIME_PRECISION_SECOND,
+			G_PARAM_READWRITE)
+	);
 }
+
+// bool
+// planified_completable_is_completed(PlanifiedCompletable* completable)
+// {
+// 	g_assert(PLANIFIED_IS_COMPLETABLE(completable));
+// 	PlanifiedCompletableInterface* iface = PLANIFIED_COMPLETABLE_GET_IFACE(completable);
+// 	g_assert(iface->is_completed != NULL);
+// 	return iface->is_completed(completable);
+// }
+//
+// void
+// planified_completable_set_completed(PlanifiedCompletable* completable, bool completed)
+// {
+// 	g_assert(PLANIFIED_IS_COMPLETABLE(completable));
+// 	PlanifiedCompletableInterface* iface = PLANIFIED_COMPLETABLE_GET_IFACE(completable);
+// 	g_assert(iface->set_completed != NULL);
+// 	iface->set_completed(completable, completed);
+// }
